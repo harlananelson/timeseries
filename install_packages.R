77a5503b5eec694cbce3af90319c1ee38deadd9b
harlananelson <harlananelson@gmail.com>
2017-09-22 10:47:56 -0400

    update

--- install_packages.R
+++ install_packages.R
@@ -1,3 +1,4 @@
 install.packages("quadprog")
 install.packages("tseries")
 install.packages('rmarkdown')
+install.packages('data.table')
--- library.R
+++ library.R
@@ -1,3 +1,4 @@
+library(data.table)
 library(quadprog)
 library(tseries)
 library(lubridate)
